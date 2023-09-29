a0318511c187597ee3825ff1c6bd9477dfffda43
Chtholly <Chtholly@HAYASEYUUKA>
2023-09-29 23:41:18 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using DSharpPlus; // Use Discord Sharp
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using HayaseBot.config;
+using System.Threading.Tasks;
+
+namespace HayaseBot
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+
+        // Bot Startup
+        static async Task Main(string[] args)
+        {
+            var caster_haruki = new caster_reader();
+            await caster_haruki.readCaster();
+
+            var config_Haruki = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = caster_haruki.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(config_Haruki);
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        // Stating the Bot Client
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ config/caster_reader.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HayaseBot.config
+{
+    internal class caster_reader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task readCaster()
+        {
+            using (StreamReader Read_ME = new StreamReader("caster.json"))
+            {
+                string read_json = await Read_ME.ReadToEndAsync();  
+                caster_structure caster_json = JsonConvert.DeserializeObject<caster_structure>(read_json);
+
+                this.token = caster_json.token;
+                this.prefix = caster_json.prefix;
+            }
+        }
+    }
+
+    internal sealed class caster_structure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+    }
+}
