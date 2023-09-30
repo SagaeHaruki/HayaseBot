3d419f30e2928b8d9c8de08229a964a2cce242cf
Hayase Yuuka <okeehnaruu@yahoo.com>
2023-10-01 00:09:36 +0900

    EVENT HANDLER AND NEW COMMANDS

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using DSharpPlus.EventArgs;
 using HayaseBot.commands;
 using HayaseBot.config;
 using System.Threading.Tasks;
+using System;
+using System.Threading.Channels;
 
 namespace HayaseBot
 {
@@ -33,6 +35,12 @@ namespace HayaseBot
             Client = new DiscordClient(config_Haruki);
             Client.Ready += Client_Ready;
 
+            // Event Handler
+            Client.MessageCreated += Event_Handler;
+
+            // FIX LATER
+            // Client.VoiceStateUpdated += VoiceChannel_Handler;
+
             // Bot Command Prefix Finder
             var config_Prefix = new CommandsNextConfiguration()
             {
@@ -51,9 +59,42 @@ namespace HayaseBot
             await Task.Delay(-1);
         }
 
+        // Voice Chat Logger (FIX NEXT)
+        /*
+        private static async Task VoiceChannel_Handler(DiscordClient sender, VoiceStateUpdateEventArgs channel_phase)
+        {
+
+                var userID = channel_phase.User.Id;
+                var userName = channel_phase.User.Username;
+                var channelID = channel_phase.Channel.Name;
+                if (channel_phase.Before == null && channel_phase.Channel.Name == channelID)
+                {
+                    Console.WriteLine("USER ID >> " + userID + " | USERNAME >> " + userName + " | ChannelID Connected >> " + channelID +" | TIME >> " + DateTime.Now);
+                }
+        }
+        */
+
+        // Message Logger
+        private static async Task Event_Handler(DiscordClient sender, MessageCreateEventArgs event_phase)
+        {
+            var userID = event_phase.Author.Id;
+            var userName = event_phase.Author.Username;
+            var channelID = event_phase.Channel.Name;
+            if (userID == 1033001102687346718)
+            {
+                // Nothing happens if the user ID is the bot (or this will loop)
+            }
+            else 
+            {
+                Console.WriteLine("USER ID >> " + userID + " | USERNAME >> " + userName + " | ChannelID " + channelID +" | TIME >> " + DateTime.Now);
+            }
+
+        }
+
         // Stating the Bot Client
         private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
         {
+            Console.WriteLine("Engine has Started");
             return Task.CompletedTask;
         }
     }
--- commands/HarukiCommands.cs
+++ commands/HarukiCommands.cs
@@ -1,27 +1,52 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace HayaseBot.commands
 {
     public class HarukiCommands : BaseCommandModule
     {
-        // Command to use
+        // Ping Command
         [Command("ping")]
 
         // Run The Command
         public async Task PingCmd(CommandContext ctx)
         {
+            // Logger
             var userID = ctx.User.Id;
             var userName = ctx.User.Username;
             
             await ctx.Channel.SendMessageAsync("pong");
-            Console.Write("UserID: | " + userID + " | UserName | " + userName + " | Used ping Command | Time: " + DateTime.Now);
+
+            // Console
+            Console.WriteLine("USER ID >> " + userID + " | USERNAME >> " + userName + " |   PING COMMAND   | TIME >> " + DateTime.Now);
+        }
+
+        // Server Info Command
+        [Command("info")]
+        public async Task ServerCmd(CommandContext ctx)
+        {
+            // Logger
+            var userID = ctx.User.Id;
+            var userName = ctx.User.Username;
+
+            var msg_toSend = new DiscordEmbedBuilder
+            {
+                Title = "Server Info",
+                Description = "<Description Here>",
+                Color = DiscordColor.Red
+            };
+            await ctx.Channel.SendMessageAsync(embed: msg_toSend);
+
+            // Console
+            Console.WriteLine("USER ID >> " + userID + " | USERNAME >> " + userName + " |   SERVER INFO COMMAND   | TIME >> " + DateTime.Now);
         }
     }
 }
