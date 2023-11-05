affbef9a05c8d55ef57526690afbd639c8268b65
Hayase Yuuka <okeehnaruu@yahoo.com>
2023-11-05 18:48:39 +0900

    Fix

--- slashcommands/InformSLCommand.cs
+++ slashcommands/InformSLCommand.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
 
 namespace HayaseBot.slashcommands
 {
@@ -38,9 +39,32 @@ namespace HayaseBot.slashcommands
                 .AddField(">ban <@username>", "Ban a member from the server", inline: false)
                 .AddField(">clear <amount>", "Clear a channel (Max 100) Cannot clear messages more than 14 days", inline: false)
                 .AddField(">timeout", "(Coming Soon)", inline: false);
-
-
             await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent(null).AddEmbed(embeder).AddComponents(Page1_Btn2, Page1_Btn));
+            return;
+        }
+
+
+        [SlashCommand("ping", "Shows the bot API Latency")]
+        public async Task PingCmd(InteractionContext ctx)
+        {
+            // Color Randomizer
+            int red = random.Next(256);
+            int green = random.Next(256);
+            int blue = random.Next(256);
+            DiscordColor randomCol = new DiscordColor((byte)red, (byte)green, (byte)blue);
+
+            // Logger
+            var userName = ctx.User.Username;
+            var ping = ctx.Client.Ping;
+
+            var embed1 = new DiscordEmbedBuilder
+            {
+                Title = "Pong!!",
+                Description = "Current API Latency is: " + ping +"ms",
+                Color = randomCol,
+                Timestamp = DateTime.UtcNow,
+            };
+            await ctx.CreateResponseAsync(embed1);
             return;
         }
     }
